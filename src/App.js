613dfb94432e69a5d6d206d039bfa380ddb7589a
Christopher Tedeschi <59670839+ChristopherTedeschi@users.noreply.github.com>
2020-02-17 19:43:59 -0500

    Navigation (#20)
    * init the project
    
    * navigation

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 import {Navbar} from "react-bootstrap";
 import {Link} from "react-router-dom";
-import {Routes} from "./Routes";
+
+import Routes from "./Routes";
+
 
 export default class App extends Component {
 render () {
 return (
   <div className = "App container">
-    <Navbar bg  = "dark" expand = "lg">
+
+    <Navbar bg  = "light" expand = "lg">
+
       <Navbar.Brand>
         <Link to= "/">Fisher Bookstore</Link>
       </Navbar.Brand>
     </Navbar>
-      <Routes />
+
+    <Routes />
+
   </div> 
 )
 
